143434ec33b149e9cb05c05a8ece9a0bbbac1bb2
Victor Volkov <vityan2504@gmail.com>
2022-03-09 16:57:53 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 'use strict';
 
+import helpers from './modules/helpers';
 import timer from './modules/timer';
 import menu from './modules/menu';
 import modal from './modules/modal';
 import scroll from './modules/scroll';
 import tabs from './modules/tabs';
 import slider from './modules/slider';
+import validation from './modules/validation';
 import calc from './modules/calc';
 
+helpers();
 timer('26 February 2022');
 menu();
 modal();
 scroll();
 tabs();
 slider('.portfolio-content', '.portfolio-item', '.portfolio-dots');
+validation();
 calc(100);
